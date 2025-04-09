cac7ac17f0a51ba61c0d888c4e641f5accfdb3f9
Ilia Stulov <iliastulov@MacBook-Pro-Ila.local>
2025-04-09 11:22:47 +0300

    feat: Add web stubs for profile, category, course, schedule, and attendance

---
+++ schoolmind/core/.ipynb_checkpoints/web_urls-checkpoint.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import web_views
+
+urlpatterns = [
+    path('profile/', web_views.profile_web_view, name='web_profile'),
+    path('category/', web_views.category_web_view, name='web_category'),
+    path('course/', web_views.course_web_view, name='web_course'),
+    path('courseschedule/', web_views.courseschedule_web_view, name='web_courseschedule'),
+    path('attendance/', web_views.attendance_web_view, name='web_attendance'),
+]
---
+++ schoolmind/core/.ipynb_checkpoints/web_views-checkpoint.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+
+def profile_web_view(request):
+    return HttpResponse("<h1>Профили</h1>")
+
+def category_web_view(request):
+    return HttpResponse("<h1>Категории</h1>")
+
+def course_web_view(request):
+    return HttpResponse("<h1>Курсы</h1>")
+
+def courseschedule_web_view(request):
+    return HttpResponse("<h1>Расписание курсов</h1>")
+
+def attendance_web_view(request):
+    return HttpResponse("<h1>Посещаемость</h1>")
